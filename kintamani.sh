7b4d128614ef0b43e09fcb8e4d43e13537cd3a55
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2013-04-15 14:44:35 +0700

    Change tab stop so the formatting won't messed up

--- kintamani.sh
+++ kintamani.sh
@@ -4,21 +4,21 @@ STATUS=$1
 
 if [ -z $STATUS ]
 then
-	echo "Sintaksis: $0 [start|stop|clean_tmp]"
+  echo "Sintaksis: $0 [start|stop|clean_tmp]"
 elif [ $STATUS == "start" ]
 then
-	nginx -p `pwd`/ -c conf/nginx.conf
+  nginx -p `pwd`/ -c conf/nginx.conf
 elif [ $STATUS == "stop" ]
 then
-	nginx -p `pwd`/ -s stop
+  nginx -p `pwd`/ -s stop
 elif [ $STATUS == "clean" ]
 then
-	rm -rf client_body_temp
-	rm -rf fastcgi_temp
-	rm -rf proxy_temp
-	rm -rf scgi_temp
-	rm -rf uwsgi_temp
-	rm -rf logs/*
+  rm -rf client_body_temp
+  rm -rf fastcgi_temp
+  rm -rf proxy_temp
+  rm -rf scgi_temp
+  rm -rf uwsgi_temp
+  rm -rf logs/*
 else
-	echo "Sintaksis: $0 [start|stop|clean]"
+  echo "Sintaksis: $0 [start|stop|clean]"
 fi
