fa815cdf67baee0492f8174fbe146ad5cf4874bf
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2013-11-30 16:41:23 +0100

    delete del-vertx-lib.sh, add deleteVertxLib gradle task

--- mods/name.bpdp~kintamani~0.0.1/del-vertx-lib.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-rm lib/hazelcast-2.6.jar
-rm lib/jackson-annotations-2.2.2.jar
-rm lib/jackson-core-2.2.2.jar
-rm lib/jackson-databind-2.2.2.jar
-rm lib/netty-all-4.0.10.Final.jar
-rm lib/vertx-core-2.0.2-final.jar
-rm lib/vertx-platform-2.0.2-final.jar
