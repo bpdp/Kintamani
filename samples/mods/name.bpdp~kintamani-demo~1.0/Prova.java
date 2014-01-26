26c0f9652c5df62c74c2c79e1c037c863ab1762f
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2014-01-26 12:38:28 +0700

    Add samples dir

---
+++ samples/mods/name.bpdp~kintamani-demo~1.0/Prova.java
@@ -0,0 +1,34 @@
+import name.bpdp.kintamani.core.*;
+
+import java.util.List;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.net.NetSocket;
+import org.vertx.java.core.streams.Pump;
+import org.vertx.java.platform.Verticle;
+import org.vertx.java.core.http.*;
+
+import ws.prova.api2.*;
+import ws.prova.exchange.*;
+
+public class Prova extends Verticle {
+
+	public void start() {
+
+		HttpServer server = vertx.createHttpServer();
+
+		RouteMatcher routeMatcher = new RouteMatcher();
+
+		String provaResults = new Server();
+
+		routeMatcher.get("/rulesinaction", new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest req) {
+				req.response().end(provaResults);
+	    }
+		});
+
+		server.requestHandler(routeMatcher).listen(8080, "localhost");
+
+	}
+
+};
